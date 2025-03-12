b94e1e6753e54900581458aac1de42d8f73ea8f7
JackFrancis-Crypto <r.cornwell86@gmail.com>
2025-03-12 18:15:17 +1100

    Testing Vercel API with simple response

--- api/test.js
+++ api/test.js
@@ -1,3 +1,3 @@
 export default function handler(req, res) {
-    res.status(200).json({ message: "Test API is working!" });
+    return res.status(200).json({ message: "Vercel API is working!" });
 }
